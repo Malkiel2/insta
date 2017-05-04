d92d14db5cd4aa010e0f8f739b40928ff63078c8
Malkiel2 <malkiel@h-MacBook-Pro-sl-Malkiel.local>
2017-05-04 16:35:59 +0300

    basic ui

---
+++ Social/SignUpVC.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpVC.swift
+//  Social
+//
+//  Created by Malkiel Shaul on 2.5.2017.
+//  Copyright © 2017 Malkiel Shaul. All rights reserved.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func dismissOnClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+
+}
